ee1ff54db7fe3539adb14a3503a70d909d6738cb
Matt Condon <mattgcondon@gmail.com>
2018-05-17 22:54:08 -0700

    fix: put graphwl route at api/graphql

--- kata-server/config/routes.rb
+++ kata-server/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'home#index'
-  post '/graphql', to: 'graphql#execute'
+  post '/api/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
