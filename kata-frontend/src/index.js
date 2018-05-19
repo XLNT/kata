ff2b6ba7c32c540a06d2f2192f7ad7d0b14012fa
Matt Condon <mattgcondon@gmail.com>
2018-05-18 17:09:01 -0700

    fix: nuke service worker

--- kata-frontend/src/index.js
+++ kata-frontend/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import 'normalize.css'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistration()
+    .then(function (reg) {
+      if (reg) {
+        reg.unregister()
+      }
+      return null
+    })
+    .catch(console.error.bind(console))
+}
