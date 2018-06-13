894425bf8bec0086d1ee374d65b185542c657c37
Matt Condon <mattgcondon@gmail.com>
2018-06-12 21:37:31 -0700

    feat: add api keys to signer and at-p

--- kata-server/app/lib/at_p.rb
+++ kata-server/app/lib/at_p.rb
@@ -1,16 +1,15 @@
 class AtP
   include HTTParty
   base_uri ENV.fetch('AT_P')
+  headers 'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': "Bearer #{ENV.fetch('AT_P_KEY')}",
 
   def self.toPlanet(ids = [])
     res = self.post('/to/planet', {
       body: {
         ids: ids,
-      }.to_json,
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
+      }.to_json
     })
     raise StandardError.new('NOPE') unless res.success?
     res['ids']
--- kata-server/app/lib/trusted_signer.rb
+++ kata-server/app/lib/trusted_signer.rb
@@ -2,13 +2,16 @@ class TrustedSigner
 
   include HTTParty
   base_uri ENV.fetch('TRUSTED_SIGNER')
+  headers 'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': "Bearer: #{ENV.fetch('TRUSTED_SIGNER_KEY')}",
 
   def self.recover(message, signature)
     res = self.post('/recover', {
       body: {
         message: message,
         signature: signature
-      },
+      }.to_json
     })
     raise StandardError.new('NOPE') unless res.success?
     res['account']
@@ -18,7 +21,7 @@ class TrustedSigner
     res = self.post('/signhash', {
       body: {
         message: message
-      }
+      }.to_json
     })
     raise StandardError.new('NOPE') unless res.success?
     res['signature']
